9262bc90ce91ac5c9bd402eb6e5cf5fd88b75af8
Yxwxwx_1 <sunxinyu347@gmail.com>
2024-11-19 20:49:15 +0800

    add 1e and 2e integral for dirac-hf

--- teach_rhf/dhf.py
+++ teach_rhf/dhf.py
@@ -1,15 +1,16 @@
 import numpy as np
-import scipy.linalg
+import scipy
 from pyscf import gto, scf
 import ctypes
 import numpy.typing as npt
+import scipy.constants
 from build import build_lib
 
 # Keep the original _cint and argtypes setup
 _cint = build_lib()
 
 argtypes = [
-    np.ctypeslib.ndpointer(dtype=np.double, ndim=2),
+    np.ctypeslib.ndpointer(dtype=np.complex128, ndim=2),
     (ctypes.c_int * 2),
     np.ctypeslib.ndpointer(dtype=np.int32, ndim=2),
     ctypes.c_int,
@@ -18,14 +19,26 @@ argtypes = [
     np.ctypeslib.ndpointer(dtype=np.double, ndim=1),
 ]
 
-_cint.CINTcgto_spheric.restype = ctypes.c_int
-_cint.CINTcgto_spheric.argtypes = [
+
+argtypes_2e = [
+    np.ctypeslib.ndpointer(dtype=np.complex128, ndim=4),
+    (ctypes.c_int * 4),
+    np.ctypeslib.ndpointer(dtype=np.intc, ndim=2),
+    ctypes.c_int,
+    np.ctypeslib.ndpointer(dtype=np.intc, ndim=2),
+    ctypes.c_int,
+    np.ctypeslib.ndpointer(dtype=np.double, ndim=1),
+    ctypes.c_void_p(0),
+]
+
+_cint.CINTcgto_spinor.restype = ctypes.c_int
+_cint.CINTcgto_spinor.argtypes = [
     ctypes.c_int,
     np.ctypeslib.ndpointer(dtype=np.int32, ndim=2),
 ]
 
-_cint.CINTtot_cgto_spheric.restype = ctypes.c_int
-_cint.CINTtot_cgto_spheric.argtypes = [
+_cint.CINTtot_cgto_spinor.restype = ctypes.c_int
+_cint.CINTtot_cgto_spinor.argtypes = [
     np.ctypeslib.ndpointer(dtype=np.int32, ndim=2),
     ctypes.c_int,
 ]
@@ -40,6 +53,8 @@ class RHF:
         self.bas = mol._bas.astype(np.intc)
         self.env = mol._env.astype(np.double)
         self.nao = mol.nao_nr()
+        self.n2c = mol.nao_2c()
+        self.n4c = self.n2c * 2
         self.nshls = len(self.bas)
         self.natm = len(self.atm)
         self.nelec = mol.nelec
@@ -47,10 +62,10 @@ class RHF:
 
         # Initialize integral matrices
         self.S = None  # Overlap matrix
-        self.T = None  # Kinetic matrix
-        self.V = None  # Nuclear attraction matrix
         self.H = None  # Core Hamiltonian
-        self.eri = None  # Electron repulsion integrals
+        self.LLLL = None
+        self.SSLL = None
+        self.SSSS = None
 
         # Nuclear repulsion energy
         self.E_nn = self._compute_nuclear_repulsion()
@@ -71,41 +86,37 @@ class RHF:
         """Precompute all necessary integrals."""
         print("Precomputing integrals...")
 
+        c = scipy.constants.speed_of_light
+        n2c = self.n2c
+        n4c = self.n4c
+
         # Setup integral function arguments
-        _cint.cint1e_ovlp_sph.argtypes = argtypes
-        _cint.cint1e_kin_sph.argtypes = argtypes
-        _cint.cint1e_nuc_sph.argtypes = argtypes
+        _cint.cint1e_spsp.argtypes = argtypes
+        _cint.cint1e_nuc.argtypes = argtypes
+        _cint.cint1e_spnucsp.argtypes = argtypes
+        _cint.cint1e_ovlp.argtypes = argtypes
 
         # Initialize matrices
-        self.S = np.zeros((self.nao, self.nao))
-        self.T = np.zeros((self.nao, self.nao))
-        self.V = np.zeros((self.nao, self.nao))
-        self.eri = np.zeros((self.nao,) * 4)
-
+        t = np.zeros((self.n2c, self.n2c), np.complex128, order="F")
+        vn = np.zeros((self.n2c, self.n2c), np.complex128, order="F")
+        wn = np.zeros((self.n2c, self.n2c), np.complex128, order="F")
+        s = np.zeros((self.n2c, self.n2c), np.complex128, order="F")
         # Compute one-electron integrals
         for i in range(self.nshls):
-            di = _cint.CINTcgto_spheric(i, self.bas)
-            x = _cint.CINTtot_cgto_spheric(self.bas, i)
+            di = _cint.CINTcgto_spinor(i, self.bas)
+            x = _cint.CINTtot_cgto_spinor(self.bas, i)
             for j in range(i, self.nshls):
-                dj = _cint.CINTcgto_spheric(j, self.bas)
-                y = _cint.CINTtot_cgto_spheric(self.bas, j)
+                dj = _cint.CINTcgto_spinor(j, self.bas)
+                y = _cint.CINTtot_cgto_spinor(self.bas, j)
 
                 # Allocate buffers
-                buf_s = np.empty((di, dj), order="F")
-                buf_t = np.empty((di, dj), order="F")
-                buf_v = np.empty((di, dj), order="F")
+                buf_t = np.empty((di, dj), np.complex128, order="F")
+                buf_vn = np.empty((di, dj), np.complex128, order="F")
+                buf_wn = np.empty((di, dj), np.complex128, order="F")
+                buf_s = np.empty((di, dj), np.complex128, order="F")
 
                 # Compute integrals
-                _cint.cint1e_ovlp_sph(
-                    buf_s,
-                    (ctypes.c_int * 2)(i, j),
-                    self.atm,
-                    self.natm,
-                    self.bas,
-                    self.nshls,
-                    self.env,
-                )
-                _cint.cint1e_kin_sph(
+                _cint.cint1e_spsp(
                     buf_t,
                     (ctypes.c_int * 2)(i, j),
                     self.atm,
@@ -114,8 +125,8 @@ class RHF:
                     self.nshls,
                     self.env,
                 )
-                _cint.cint1e_nuc_sph(
-                    buf_v,
+                _cint.cint1e_nuc(
+                    buf_vn,
                     (ctypes.c_int * 2)(i, j),
                     self.atm,
                     self.natm,
@@ -123,94 +134,105 @@ class RHF:
                     self.nshls,
                     self.env,
                 )
-
+                _cint.cint1e_spnucsp(
+                    buf_wn,
+                    (ctypes.c_int * 2)(i, j),
+                    self.atm,
+                    self.natm,
+                    self.bas,
+                    self.nshls,
+                    self.env,
+                )
+                _cint.cint1e_ovlp(
+                    buf_s,
+                    (ctypes.c_int * 2)(i, j),
+                    self.atm,
+                    self.natm,
+                    self.bas,
+                    self.nshls,
+                    self.env,
+                )
                 # Store results
-                self.S[x : x + di, y : y + dj] = buf_s
-                self.T[x : x + di, y : y + dj] = buf_t
-                self.V[x : x + di, y : y + dj] = buf_v
+                t[x : x + di, y : y + dj] = buf_t
+                vn[x : x + di, y : y + dj] = buf_vn
+                wn[x : x + di, y : y + dj] = buf_wn
+                s[x : x + di, y : y + dj] = buf_s
 
-                self.S[y : y + dj, x : x + di] = buf_s.T
-                self.T[y : y + dj, x : x + di] = buf_t.T
-                self.V[y : y + dj, x : x + di] = buf_v.T
+                t[y : y + dj, x : x + di] = buf_t.conj().T
+                vn[y : y + dj, x : x + di] = buf_vn.conj().T
+                wn[y : y + dj, x : x + di] = buf_wn.conj().T
+                s[y : y + dj, x : x + di] = buf_s.conj().T
+
+        self.H = np.empty((n4c, n4c), np.complex128)
+        self.H[:n2c, :n2c] = vn
+        self.H[n2c:, :n2c] = t * 0.5
+        self.H[:n2c, n2c:] = t * 0.5
+        self.H[n2c:, n2c:] = wn * (0.25 / c**2) - t * 0.5
+
+        self.S = np.empty((n4c, n4c), np.complex128)
+        self.S[:n2c, :n2c] = s
+        self.S[n2c:, n2c:] = t * (0.5 / c) ** 2
 
         # Compute two-electron integrals
         print("Computing ERI integrals...")
-        CINTOpt = ctypes.c_void_p
-        opt = CINTOpt()
+        _cint.cint2e.argtypes = argtypes_2e
+        _cint.cint2e_spsp1.argtypes = argtypes_2e
+        _cint.cint2e_spsp1spsp2.argtypes = argtypes_2e
 
-        _cint.cint2e_sph_optimizer.argtypes = [
-            ctypes.POINTER(CINTOpt),
-            np.ctypeslib.ndpointer(dtype=np.intc, ndim=2),
-            ctypes.c_int,
-            np.ctypeslib.ndpointer(dtype=np.intc, ndim=2),
-            ctypes.c_int,
-            np.ctypeslib.ndpointer(dtype=np.double, ndim=1),
-        ]
+        self.LLLL = np.empty((n2c, n2c, n2c, n2c), np.complex128)
+        self.SSLL = np.empty((n2c, n2c, n2c, n2c), np.complex128)
+        self.SSSS = np.empty((n2c, n2c, n2c, n2c), np.complex128)
 
-        _cint.cint2e_sph_optimizer(
-            ctypes.byref(opt), self.atm, self.natm, self.bas, self.nshls, self.env
-        )
-        _cint.cint2e_sph.argtypes = [
-            np.ctypeslib.ndpointer(dtype=np.double, ndim=4),
-            (ctypes.c_int * 4),
-            np.ctypeslib.ndpointer(dtype=np.intc, ndim=2),
-            ctypes.c_int,
-            np.ctypeslib.ndpointer(dtype=np.intc, ndim=2),
-            ctypes.c_int,
-            np.ctypeslib.ndpointer(dtype=np.double, ndim=1),
-            CINTOpt,
-        ]
         for i in range(self.nshls):
-            di = _cint.CINTcgto_spheric(i, self.bas)
-            x = _cint.CINTtot_cgto_spheric(self.bas, i)
-            for j in range(i, self.nshls):
-                dj = _cint.CINTcgto_spheric(j, self.bas)
-                y = _cint.CINTtot_cgto_spheric(self.bas, j)
+            di = _cint.CINTcgto_spinor(i, self.bas)
+            x = _cint.CINTtot_cgto_spinor(self.bas, i)
+            for j in range(self.nshls):
+                dj = _cint.CINTcgto_spinor(j, self.bas)
+                y = _cint.CINTtot_cgto_spinor(self.bas, j)
                 for k in range(self.nshls):
-                    dk = _cint.CINTcgto_spheric(k, self.bas)
-                    z = _cint.CINTtot_cgto_spheric(self.bas, k)
-                    for l in range(k, self.nshls):  # noqa: E741
-                        dl = _cint.CINTcgto_spheric(l, self.bas)
-                        w = _cint.CINTtot_cgto_spheric(self.bas, l)
+                    dk = _cint.CINTcgto_spinor(k, self.bas)
+                    z = _cint.CINTtot_cgto_spinor(self.bas, k)
+                    for l in range(self.nshls):  # noqa: E741
+                        dl = _cint.CINTcgto_spinor(l, self.bas)
+                        w = _cint.CINTtot_cgto_spinor(self.bas, l)
 
-                        buf = np.empty((di, dj, dk, dl), order="F")
-                        _cint.cint2e_sph(
-                            buf,
+                        llll = np.empty((di, dj, dk, dl), np.complex128, order="F")
+                        ssll = np.empty((di, dj, dk, dl), np.complex128, order="F")
+                        ssss = np.empty((di, dj, dk, dl), np.complex128, order="F")
+                        _cint.cint2e(
+                            llll,
                             (ctypes.c_int * 4)(i, j, k, l),
                             self.atm,
                             self.natm,
                             self.bas,
                             self.nshls,
                             self.env,
-                            opt,
+                            ctypes.c_void_p(0),
                         )
-                        self.eri[x : x + di, y : y + dj, z : z + dk, w : w + dl] = (
-                            buf.transpose(0, 1, 2, 3)
+                        _cint.cint2e_spsp1(
+                            ssll,
+                            (ctypes.c_int * 4)(i, j, k, l),
+                            self.atm,
+                            self.natm,
+                            self.bas,
+                            self.nshls,
+                            self.env,
+                            ctypes.c_void_p(0),
                         )
-                        self.eri[y : y + dj, x : x + di, z : z + dk, w : w + dl] = (
-                            buf.transpose(1, 0, 2, 3)
+                        _cint.cint2e_spsp1spsp2(
+                            ssss,
+                            (ctypes.c_int * 4)(i, j, k, l),
+                            self.atm,
+                            self.natm,
+                            self.bas,
+                            self.nshls,
+                            self.env,
+                            ctypes.c_void_p(0),
                         )
-                        self.eri[x : x + di, y : y + dj, w : w + dl, z : z + dk] = (
-                            buf.transpose(0, 1, 3, 2)
-                        )
-                        self.eri[y : y + dj, x : x + di, w : w + dl, z : z + dk] = (
-                            buf.transpose(1, 0, 3, 2)
-                        )
-                        self.eri[z : z + dk, w : w + dl, x : x + di, y : y + dj] = (
-                            buf.transpose(2, 3, 0, 1)
-                        )
-                        self.eri[w : w + dl, z : z + dk, x : x + di, y : y + dj] = (
-                            buf.transpose(3, 2, 0, 1)
-                        )
-                        self.eri[z : z + dk, w : w + dl, y : y + dj, x : x + di] = (
-                            buf.transpose(2, 3, 1, 0)
-                        )
-                        self.eri[w : w + dl, z : z + dk, y : y + dj, x : x + di] = (
-                            buf.transpose(3, 2, 1, 0)
-                        )
-
+                        self.LLLL[x : x + di, y : y + dj, z : z + dk, w : w + dl] = llll
+                        self.SSLL[x : x + di, y : y + dj, z : z + dk, w : w + dl] = ssll
+                        self.SSSS[x : x + di, y : y + dj, z : z + dk, w : w + dl] = ssss
         # Compute core Hamiltonian and orthogonalization matrix
-        self.H = self.T + self.V
         print("Integral computation completed.")
 
     def get_fock(self, D: npt.NDArray) -> npt.NDArray:
@@ -305,22 +327,29 @@ def main():
     }
 
     mf = RHF(mol)
-    print("atm:\n", mol._atm)
-    print("bas:\n", mol._bas)
-    print("env:\n", mol._env)
+    mf._compute_all_integrals()
 
+    ref = mol.intor("int2e_spsp1spsp2_spinor").reshape(mf.n2c, mf.n2c, mf.n2c, mf.n2c)
+
+    # print(np.sum(ref - mf.SSSS))
     # Compare with PySCF
-    mf_pyscf = scf.RHF(mol)
-    E_pyscf = mf_pyscf.kernel()
-    print(f"\nPySCF energy: {E_pyscf:.10f}")
+    # mf_pyscf = scf.DHF(mol)
+    # mf_pyscf.verbose = 5
+    # print("E(Dirac-Coulomb) = %.15g" % mf_pyscf.kernel())
 
+    # mf_pyscf.with_gaunt = True
+    # print("E(Dirac-Coulomb-Gaunt) = %.15g" % mf_pyscf.kernel())
 
-#
-# # Our implementation
-# mf = RHF(mol)
-# E_our = mf.kernel()
-# print(f"Our energy:   {E_our:.10f}")
-# print(f"Difference:   {abs(E_pyscf - E_our):.10f}")
+    #
+    # mf_pyscf.with_breit = True
+    # print("E(Dirac-Coulomb-Breit) = %.15g" % mf_pyscf.kernel())
+
+    #
+    # # Our implementation
+    # mf = RHF(mol)
+    # E_our = mf.kernel()
+    # print(f"Our energy:   {E_our:.10f}")
+    # print(f"Difference:   {abs(E_pyscf - E_our):.10f}")
 
 
 if __name__ == "__main__":
