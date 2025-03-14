67ea8604c7e2f093e49903f4bb6f12251b8f871f
Yxwxwx_1 <sunxinyu347@gmail.com>
2025-03-14 16:35:11 +0800

    rewrite slater_condon

--- slater_condon/scripts/creat_fcidump_pyscf.py
+++ slater_condon/scripts/creat_fcidump_pyscf.py
@@ -1,10 +1,13 @@
-from pyscf import gto, mcscf
+from pyscf import gto, mcscf, fci
 from pyscf.tools import fcidump
 
 mol = gto.M(atom="n 0 0 -0.55; n 0 0 0.55", basis="sto-3g")
 myhf = mol.RHF().run()
 
-ncas, nelecas = (6, 6)
-mycas = mcscf.CASCI(myhf, ncas, nelecas)
+cisolver = fci.FCI(myhf)
+print("E(FCI) = %.12f" % cisolver.kernel()[0])
+
+# ncas, nelecas = (10, 14)
+# mycas = mcscf.CASCI(myhf, ncas, nelecas)
 # mycas.kernel()
-fcidump.from_mcscf(mycas, "fcidump.example1")
+# fcidump.from_mcscf(mycas, "fcidump.example1")
