6ec5893d8b56493ac86bb303c6922faae44a0e5f
jaa2024 <su@LAPTOP-VN88DF0I.>
2025-06-11 00:34:41 +0800

    add example

--- slater_condon/main.cpp
+++ slater_condon/main.cpp
@@ -6,8 +6,8 @@
 int main()
 {
 
-    integral::Integral<> MO_INTEGRAL("../example/n2_fci/fcidump.example1");
-    ci::SlaterCondon<double> SC("../example/n2_fci/electron_configurations.txt");
+    integral::Integral<> MO_INTEGRAL("../example/c2_cas88/FCIDUMP");
+    ci::SlaterCondon<double> SC("../example/c2_cas88/electron_configurations.txt");
     SC.kernel(MO_INTEGRAL, 6, 6);
     return 0;
 }
